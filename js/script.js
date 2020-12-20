165814102aa698810e53aca3660fe3e941a7dab2
Adekunle Adedokun <adekunleaedokun6@gmail.com>
2020-12-20 10:32:50 +0100

    New Commit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var menuToggle = document.querySelector('.hamburger-wrapper');
 var Burger = document.querySelector('.hamburger');
 var Nav = document.querySelector('.nav');
 var navList = document.querySelector('.nav-list');
+var menuItems =  document.querySelector('.nav-link');
 
 
 menuToggle.addEventListener('click', Toggler);
@@ -15,8 +16,8 @@ function Toggler() {
         navList.classList.add('show');
         menuDisplay = true;
     } else {
-        menuToggle.classList.remove('fixer');
         Burger.classList.remove('open');
+        menuToggle.classList.remove('fixer');
         Nav.classList.remove('color-change');
         navList.classList.remove('show');
         menuDisplay = false;
